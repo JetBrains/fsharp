6ca07209d40c0a76cd31663c63eaa01ca082df2a
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2025-11-03 17:52:22 +0100

    Type checker: recover on checking binding parameter contraints

--- tests/FSharp.Compiler.Service.Tests/TypeChecker/TypeCheckerRecoveryTests.fs
+++ tests/FSharp.Compiler.Service.Tests/TypeChecker/TypeCheckerRecoveryTests.fs
@@ -100,3 +100,22 @@ if true then
     o.GetHashCode{caret}
 """
 
+module Constraints =
+    [<Fact>]
+    let ``Type 01`` () =
+        assertHasSymbolUsageAtCaret "f" """
+let f (x: string) =
+    x + 1
+
+{caret}f ""
+"""
+
+    [<Fact>]
+    let ``Type 02`` () =
+        assertHasSymbolUsageAtCaret "M" """
+type T =
+    static member M(x: string) =
+        x + 1
+
+T.M{caret} ""
+"""
