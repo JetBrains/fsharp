99d317af8b6c1ba4f6ecfc4a3572eee627bb258e
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2025-11-03 14:32:07 +0100

    Squashed commit of the following:
    commit cd1f270e945be2a1682ef6a0c11d1facd68d70f2
    Author: Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
    Date:   Mon Feb 16 17:44:59 2026 +0100
    
        Fantomas
    
    commit 4edb3dce8de1b2e313f5bbd14adcf1afcfc3d26f
    Author: Eugene Auduchinok <eugene.auduchinok@gmail.com>
    Date:   Mon Feb 16 17:12:08 2026 +0100
    
        Release notes
    
    commit b737ebc63aaee4a5501cdcb5deaf8ee49265a023
    Author: Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
    Date:   Mon Feb 16 16:54:21 2026 +0100
    
        FCS: capture additional types during analysis
    
    commit e82d6f66c935e4e21396c9018a94c3f218d7749f
    Author: Tomas Grosup <Tomas.Grosup@gmail.com>
    Date:   Mon Feb 16 12:56:35 2026 +0100
    
        Create SKILL.md (#19282)
    
    commit 01bad01dfd405c8f623dc0b934aa661fd532c9b4
    Author: Tomas Grosup <Tomas.Grosup@gmail.com>
    Date:   Thu Feb 12 21:49:32 2026 +0100
    
        Find All References and Rename Symbol Bug Fixes (#19252)
    FCS: add more predefined types checks

--- src/Compiler/Symbols/Symbols.fs
+++ src/Compiler/Symbols/Symbols.fs
@@ -2571,24 +2571,55 @@ type FSharpType(cenv, ty:TType) =
 
     member _.IsMeasureType =
        isResolved() &&
-       protect <| fun () ->
-        match stripTyparEqns ty with
-        | TType_measure _ -> true
-        | _ -> false
+       protect <| fun () -> isMeasureTy cenv.g ty
 
     member _.IsTupleType = 
        isResolved() &&
-       protect <| fun () -> 
-        match stripTyparEqns ty with 
-        | TType_tuple _ -> true 
-        | _ -> false
+       protect <| fun () -> isRefTupleTy cenv.g ty
 
     member _.IsStructTupleType = 
        isResolved() &&
-       protect <| fun () -> 
-        match stripTyparEqns ty with 
-        | TType_tuple (tupInfo, _) -> evalTupInfoIsStruct tupInfo
-        | _ -> false
+       protect <| fun () -> isStructTupleTy cenv.g ty
+
+    member _.IsUnitType =
+        isResolved () &&
+        protect <| fun () -> isUnitTy cenv.g ty
+
+    member _.IsArrayType =
+        isResolved () &&
+        protect <| fun () -> isArrayTy cenv.g ty
+
+    member _.IsNativePointerType =
+        isResolved () &&
+        protect <| fun () -> isNativePtrTy cenv.g ty
+
+    member _.IsFSharpList =
+        isResolved () &&
+        protect <| fun () -> isListTy cenv.g ty
+
+    member _.IsFSharpChoice =
+        isResolved () &&
+        protect <| fun () -> isChoiceTy cenv.g ty
+
+    member _.IsFSharpOption =
+        isResolved () &&
+        protect <| fun () -> isOptionTy cenv.g ty
+
+    member _.IsFSharpValueOption =
+        isResolved () &&
+        protect <| fun () -> isValueOptionTy cenv.g ty
+
+    member _.IsStringType =
+        isResolved () &&
+        protect <| fun () -> isStringTy cenv.g ty
+
+    member _.IsObjectType =
+        isResolved () &&
+        protect <| fun () -> isObjTyAnyNullness cenv.g ty
+
+    member _.IsBooleanType =
+        isResolved () &&
+        protect <| fun () -> isBoolTy cenv.g ty
 
     member _.TypeDefinition = 
        protect <| fun () -> 
@@ -2647,17 +2678,11 @@ type FSharpType(cenv, ty:TType) =
 
     member _.IsFunctionType = 
        isResolved() &&
-       protect <| fun () -> 
-        match stripTyparEqns ty with 
-        | TType_fun _ -> true 
-        | _ -> false
+       protect <| fun () -> isFunTy cenv.g ty
 
     member _.IsAnonRecordType = 
        isResolved() &&
-       protect <| fun () -> 
-        match stripTyparEqns ty with 
-        | TType_anon _ -> true 
-        | _ -> false
+       protect <| fun () -> isAnonRecdTy cenv.g ty
 
     member _.AnonRecordTypeDetails = 
        protect <| fun () -> 
@@ -2690,17 +2715,18 @@ type FSharpType(cenv, ty:TType) =
         GetSuperTypeOfType cenv.g cenv.amap range0 ty
         |> Option.map (fun ty -> FSharpType(cenv, ty)) 
 
-    member x.ErasedType=
+    member x.ErasedType =
         FSharpType(cenv, stripTyEqnsWrtErasure EraseAll cenv.g ty)
 
     member x.BasicQualifiedName =
-        protect <| fun () ->
-            match stripTyparEqns ty with 
-            | TType_app(tcref, _, _) ->
-                match tcref.CompiledRepresentation with 
-                | CompiledTypeRepr.ILAsmNamed(tref, _, _) -> Some tref.BasicQualifiedName
-                | CompiledTypeRepr.ILAsmOpen _ -> None 
-            | _ -> None
+        if isUnresolved () then None else
+
+        match stripTyparEqns ty with 
+        | TType_app(tcref, _, _) ->
+            match tcref.CompiledRepresentation with 
+            | CompiledTypeRepr.ILAsmNamed(tref, _, _) -> Some tref.BasicQualifiedName
+            | CompiledTypeRepr.ILAsmOpen _ -> None 
+        | _ -> None
 
     member _.Instantiate(instantiation:(FSharpGenericParameter * FSharpType) list) = 
         let resTy = instType (instantiation |> List.map (fun (tyv, ty) -> tyv.TypeParameter, ty.Type)) ty
